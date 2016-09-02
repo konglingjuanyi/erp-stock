7835ea4af2284107f0ede50fb3a1032daa8eb939
yubinbin <598628744@qq.com>
2016-09-02 16:39:59 +0800

    更新

--- stock-service/src/test/java/com/cn/test/TestWarehouse.java
+++ stock-service/src/test/java/com/cn/test/TestWarehouse.java
@@ -8,29 +8,18 @@ import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.jiji.erp.dal.SerialNumberDAO;
-import com.jiji.erp.stock.api.WarehouseService;
-import com.jiji.erp.stock.model.WarehouseDO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextHierarchy(@ContextConfiguration({ "classpath*:applicationContext-service.xml" }))
 public class TestWarehouse {
-
-	@Autowired
-	WarehouseService warehouseService;
     @Autowired
     SerialNumberDAO serialNumberDAO;
     
 	@Test
 	public void testInsert()throws Exception{
-		 System.out.println("==test==");
-		 for(int i=0;i<10;i++){
-			 WarehouseDO warehouseDO=new WarehouseDO();
-			 warehouseDO.setAreaId(1l);
-			 warehouseDO.setAreaName("daf"+i);
-			 warehouseDO.setCode(serialNumberDAO.getSerialNumberByDate("DH","warehouse"));
-			 warehouseDO.setName("yubin");
-		     warehouseService.insert(warehouseDO);
-		 } 
+		
+		 System.out.println("==test=="+serialNumberDAO.getSerialNumberByDate("DH","warehouse"));
+		 
 	}
 	 
 }
